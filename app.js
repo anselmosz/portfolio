7023965dd016a91bb62e69b0ccd331342ddaa1aa
Matheus S. Anselmo <169082718+Anselmo-Ma@users.noreply.github.com>
2024-10-11 13:38:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const $html = document.querySelector("html")
-const $theme = document.getElementById("theme-selector")
+const $html = document.querySelector("html");
+const $theme = document.getElementById("theme-selector");
+
+$theme.addEventListener("click", function() {
+  $html.classList.toggle("light-mode");
+  $modo.classList("light");
+});
