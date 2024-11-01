b7cff9d094c986db9f262188a7249980210764d0
Anselmo <anselmoma2005@outlook.com>
2024-11-01 20:47:28 -0300

    Update repo by removing files

--- app.js
+++
@@ -1,7 +0,0 @@
-const $html = document.querySelector("html");
-const $theme = document.getElementById("theme-selector");
-
-$theme.addEventListener("click", function() {
-  $html.classList.toggle("light-mode");
-  $modo.classList("light");
-});
