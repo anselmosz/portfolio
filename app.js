9d824f89d4bc3e7a62425394c81326600aec16b5
Anselmo <anselmoma2005@outlook.com>
2024-11-12 17:14:16 -0300

    update color pallete

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ $modo.addEventListener("click", function () {
 
   $modo.classList.toggle("dark-mode");
   const imagem = document.getElementById("imagem-tema");
+  const logo = document.getElementById("logo-src");
+
   if ($modo.classList.contains("dark-mode")) {
-    imagem.src = "./images/typing-bro-blue.png";
+    imagem.src = "./images/typing-bro-red.png";
+    logo.src = "./icons/logo-dark.png";
   } else {
-      imagem.src = "./images/typing-bro-red.png";
+    imagem.src = "./images/typing-bro-blue.png";
+    logo.src = "./icons/logo-white.png";
   }
 });
